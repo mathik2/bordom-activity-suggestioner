4b2ef97daf3f6663d40435dd895ae2a3b4e2cff6
Mathivathana S <45286002+mathik2@users.noreply.github.com>
2021-11-11 16:42:14 +0530

    seperate css and js files (#1)

---
+++ activity.js
@@ -0,0 +1,20 @@
+ //Accesccing html element using DOM
+ const topic = document.getElementById('suggestedTopic');
+ const activityText = document.getElementById('activity');
+ /*
+ 1)By using async with fetch api store fetched data in variable 'data' 
+ 2)await keyword makes JavaScript runtime to pause code on this line not allowing
+ further code to execute  until the async function returned its result
+ 3) json() that lets you extract a JSON object from the response
+ */
+ async function activities() {
+     var response = await fetch('https://www.boredapi.com/api/activity')
+     var data = await response.json();
+     return data;
+
+ }
+ //After that using promise ,we can retrive data
+ activities().then(data => {
+     topic.innerHTML = "Try this   " + data.type + "  activity"
+     activityText.innerHTML = data.activity;
+ })
